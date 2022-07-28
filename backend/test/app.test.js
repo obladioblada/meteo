0293a2d90b91e346f7964a366221d52900963712
gianpaolo <gianpaolo@addsearch.com>
2022-07-28 22:59:39 +0300

    added tests

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -1,12 +1,30 @@
 const request = require('supertest')
 const app = require('../app')
 
-describe('Status', () => {
-  it('stest server reachable', async () => {
+describe('server status', () => {
+  it('status ok', async () => {
     const res = await request(app)
             .get('/api/status')
             .send()
     expect(res.statusCode).toEqual(200)
     expect(res.text).toEqual('ok')
+  })
+})
+
+describe('no location parameter', () => {
+  it('bad request returned', async () => {
+    const res = await request(app)
+            .get('/api/meteo')
+            .send()
+    expect(res.statusCode).toEqual(400)
+  })
+})
+
+describe('parse weather response', () => {
+  it('response returned', async () => {
+    const res = await request(app)
+            .get('/api/meteo').query({location: 'rome'})
+            .send()
+    expect(res.statusCode).toEqual(200)
   })
 })
